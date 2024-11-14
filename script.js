10e022d3d09dbafc3490ce394f12ce406d21d0a6
schalchlincode <schalchlincode@gmail.com>
2024-11-14 00:14:01 -0600

    Add function to randomly generate rock, paper, or scissors

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+  const randomNum = Math.floor(Math.random() * 3);
+
+  if (randomNum === 0) {
+    return "rock";
+  } else if (randomNum === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
