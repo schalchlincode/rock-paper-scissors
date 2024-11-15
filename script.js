52b001c722bd37521ba60e121f0e0c9373f9e149
schalchlincode <schalchlincode@gmail.com>
2024-11-14 21:47:32 -0600

    Add playRound function to play one round of the game

--- script.js
+++ script.js
@@ -10,12 +10,38 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
   let choice = prompt("Choose Rock, Paper or Scissors");
 
-  console.log(choice);
+  return choice.toLowerCase();
 }
 
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log("you win!");
+  } else if (
+    (humanChoice === "paper" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "rock") ||
+    (humanChoice === "rock" && computerChoice === "paper")
+  ) {
+    console.log("you lose!");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "rock") ||
+    (humanChoice === "paper" && computerChoice === "paper") ||
+    (humanChoice === "scissors" && computerChoice === "scissors")
+  ) {
+    console.log("you tied");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("Human choice:", humanSelection);
+console.log("Computer choice:", computerSelection);
+
+playRound(humanSelection, computerSelection);
