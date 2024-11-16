ae940ca149b84ac772a006f0f347930f61119fd1
schalchlincode <schalchlincode@gmail.com>
2024-11-15 20:19:47 -0600

    Add score variables and increment code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// gets the computer's choice
 function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * 3);
 
@@ -10,11 +11,15 @@ function getComputerChoice() {
   }
 }
 
+// gets human choice
 function getHumanChoice() {
   let choice = prompt("Choose Rock, Paper or Scissors");
 
   return choice.toLowerCase();
 }
+
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
   if (
@@ -23,12 +28,14 @@ function playRound(humanChoice, computerChoice) {
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
     console.log("you win!");
+    humanScore++;
   } else if (
     (humanChoice === "paper" && computerChoice === "scissors") ||
     (humanChoice === "scissors" && computerChoice === "rock") ||
     (humanChoice === "rock" && computerChoice === "paper")
   ) {
     console.log("you lose!");
+    computerScore++;
   } else if (
     (humanChoice === "rock" && computerChoice === "rock") ||
     (humanChoice === "paper" && computerChoice === "paper") ||
@@ -38,10 +45,12 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice(); //assigns humans choice to humanSelection
+const computerSelection = getComputerChoice(); //assigsn computer choice to computerSelection
+
+playRound(humanSelection, computerSelection);
 
 console.log("Human choice:", humanSelection);
 console.log("Computer choice:", computerSelection);
-
-playRound(humanSelection, computerSelection);
+console.log("Human Score: ", humanScore); //must be called after playRound
+console.log("Computer Score: ", computerScore); //must be called after playRound
