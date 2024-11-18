a25392f2cc9ad9d6b5be7b6e50edfd2e6a38d754
schalchlincode <schalchlincode@gmail.com>
2024-11-17 21:10:47 -0600

    Add 5-round gameplay to playRound function within playGame

--- script.js
+++ script.js
@@ -18,39 +18,44 @@ function getHumanChoice() {
   return choice.toLowerCase();
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-  if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "scissors" && computerChoice === "paper")
-  ) {
-    console.log("you win!");
-    humanScore++;
-  } else if (
-    (humanChoice === "paper" && computerChoice === "scissors") ||
-    (humanChoice === "scissors" && computerChoice === "rock") ||
-    (humanChoice === "rock" && computerChoice === "paper")
-  ) {
-    console.log("you lose!");
-    computerScore++;
-  } else if (
-    (humanChoice === "rock" && computerChoice === "rock") ||
-    (humanChoice === "paper" && computerChoice === "paper") ||
-    (humanChoice === "scissors" && computerChoice === "scissors")
-  ) {
-    console.log("you tied");
+  function playRound(humanChoice, computerChoice) {
+    if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      console.log("you win!");
+      humanScore++;
+    } else if (
+      (humanChoice === "paper" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "rock") ||
+      (humanChoice === "rock" && computerChoice === "paper")
+    ) {
+      console.log("you lose!");
+      computerScore++;
+    } else if (
+      (humanChoice === "rock" && computerChoice === "rock") ||
+      (humanChoice === "paper" && computerChoice === "paper") ||
+      (humanChoice === "scissors" && computerChoice === "scissors")
+    ) {
+      console.log("you tied");
+    }
+
+    console.log("Human choice:", humanChoice);
+    console.log("Computer choice:", computerChoice);
+    console.log("Human Score: ", humanScore);
+    console.log("Computer Score: ", computerScore);
+  }
+  for (i = 0; i < 5; i++) {
+    let humanSelection = getHumanChoice(); //assigns humans choice to humanSelection
+    let computerSelection = getComputerChoice(); //assigsn computer choice to computerSelection
+
+    playRound(humanSelection, computerSelection);
   }
 }
 
-const humanSelection = getHumanChoice(); //assigns humans choice to humanSelection
-const computerSelection = getComputerChoice(); //assigsn computer choice to computerSelection
-
-playRound(humanSelection, computerSelection);
-
-console.log("Human choice:", humanSelection);
-console.log("Computer choice:", computerSelection);
-console.log("Human Score: ", humanScore); //must be called after playRound
-console.log("Computer Score: ", computerScore); //must be called after playRound
+playGame();
